0b70552d56ab42fa2f14a9cee0f077e4d3917659
Zhiwei Xue <zwxue@talend.com>
2020-12-31 12:13:26 +0800

    feat(TUP-29019): Build routine jars in different maven projects

--- main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/ui/wizards/CamelNewInnerBeanWizard.java
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/ui/wizards/CamelNewInnerBeanWizard.java
@@ -12,19 +12,37 @@
 // ============================================================================
 package org.talend.camel.designer.ui.wizards;
 
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.core.runtime.IPath;
+import org.talend.core.model.properties.ByteArray;
 import org.talend.core.model.properties.Property;
+import org.talend.core.model.repository.ERepositoryObjectType;
 import org.talend.core.model.routines.RoutinesUtil;
+import org.talend.designer.maven.utils.CodesJarMavenUtil;
+import org.talend.repository.model.RepositoryNode;
 
 public class CamelNewInnerBeanWizard extends CamelNewBeanWizard {
 
-    public CamelNewInnerBeanWizard(IPath path) {
+    private RepositoryNode node;
+
+    public CamelNewInnerBeanWizard(RepositoryNode node, IPath path) {
         super(path);
+        this.node = node;
+        setUpCodePackage();
+    }
+
+    protected void setUpCodePackage() {
+        String gavPackage = StringUtils
+                .replace(CodesJarMavenUtil.getGAVPackageForCodesJar(node.getObject().getProperty().getItem()), "/", ".");
+        gavPackage = "package " + gavPackage + ";";
+        ByteArray contents = beanItem.getContent();
+        String newInnerContent = StringUtils.replaceOnce(new String(contents.getInnerContent()), "package beans;", gavPackage);
+        contents.setInnerContent(newInnerContent.getBytes());
     }
 
     @Override
     protected void addAdditionalProperties(Property property) {
-        RoutinesUtil.setInnerCodes(property, true);
+        RoutinesUtil.setInnerCodes(property, ERepositoryObjectType.BEANSJAR);
     }
 
 }

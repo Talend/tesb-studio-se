1a5352b3345d94ae1f8641956b53b5c8a2cd75a7
Jane Ding <jding@talend.com>
2021-01-17 19:12:53 +0800

    feat(TUP-29943):import inner beans to change package name and import (#1035)
    * feat(TUP-29943):import inner beans to change package name and import
    beansJar with lib
    https://jira.talendforge.org/browse/TUP-29943
    
    Signed-off-by: jding-tlnd <jding@talend.com>
    
    * feat(TUP-29943):create beansJar and inner beans import handler
    https://jira.talendforge.org/browse/TUP-29943
    Signed-off-by: jding-tlnd <jding@talend.com>

--- main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/ui/view/handler/BeansJarImportHandler.java
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/ui/view/handler/BeansJarImportHandler.java
@@ -12,9 +12,17 @@
 // ============================================================================
 package org.talend.camel.designer.ui.view.handler;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.talend.commons.CommonsPlugin;
 import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.core.model.routines.RoutinesUtil;
+import org.talend.core.repository.utils.RoutineUtils;
+import org.talend.core.runtime.CoreRuntimePlugin;
+import org.talend.core.utils.ProductUtils;
+import org.talend.librariesmanager.model.ModulesNeededProvider;
 import org.talend.repository.items.importexport.handlers.imports.ImportRepTypeHandler;
 import org.talend.repository.items.importexport.handlers.model.ImportItem;
+import org.talend.repository.items.importexport.manager.ResourcesManager;
 
 /**
  * DOC jding  class global comment. Detailled comment
@@ -25,16 +33,66 @@ public class BeansJarImportHandler extends ImportRepTypeHandler {
         super();
     }
 
+    /**
+     * override to use this handler import BeansJar and inner Beans
+     */
     @Override
     public boolean valid(ImportItem importItem) {
-        boolean valid = super.valid(importItem);
-        if (valid) {
-            ERepositoryObjectType itemType = importItem.getType();
-            if (itemType != null && itemType == ERepositoryObjectType.BEANSJAR) {
-                return true;
-            }
+        if (importItem == null || importItem.getItem() == null || importItem.getType() == null) {
+            return false;
         }
+        ERepositoryObjectType itemType = importItem.getType();
+        if (itemType == ERepositoryObjectType.BEANSJAR || RoutinesUtil.isInnerCodes(importItem.getProperty())) {
+            // if in studio, check the product.
+            if (!CommonsPlugin.isHeadless() && isEnableProductChecking()) {
+                String currentPerspectiveId = CoreRuntimePlugin.getInstance().getActivedPerspectiveId();
+                String[] products = itemType.getProducts();
+                if (products != null) {
+                    for (String product : products) {
+                        String perspectiveId = ProductUtils.getPerspectiveId(product);
+                        if (currentPerspectiveId != null && currentPerspectiveId.equals(perspectiveId)) {
+                            return true; // match the product and perspective.
+                        }
+                    }
+                    return false; // if enable product check, have set the product.
+                }
+            }
+            return true;
+        }
+
         return false;
+    }
+
+    /*
+     * Update import bean libraries version with the studio inside versions.(TESB-23162)
+     *
+     * @see
+     * org.talend.repository.items.importexport.handlers.imports.ImportBasicHandler#afterImportingItems(org.eclipse.core
+     * .runtime.IProgressMonitor, org.talend.repository.items.importexport.manager.ResourcesManager,
+     * org.talend.repository.items.importexport.handlers.model.ImportItem)
+     */
+    @Override
+    public void afterImportingItems(IProgressMonitor monitor, ResourcesManager resManager, ImportItem importItem) {
+        ModulesNeededProvider.fireLibrariesChanges();
+    }
+
+    /*
+     * Update import bean libraries version with the studio inside versions.(TESB-23162)
+     */
+    @Override
+    protected void beforeCreatingItem(ImportItem importItem) {
+        super.beforeCreatingItem(importItem);
+        // inner Beans to change package name
+        if (RoutinesUtil.isInnerCodes(importItem.getProperty())) {
+            RoutineUtils.changeInnerCodePackage(importItem.getItem(), true);
+            return;
+        }
+
+        // import jar for BeansJar
+//        if (importItem != null && importItem.getItem() != null && importItem.getItem() instanceof BeansJarItem) {
+//            EList<IMPORTType> imports = ((BeansJarItem) importItem.getItem()).getRoutinesJarType().getImports();
+//            BeansImportHandlerUtil.arrangeBeansMoudulesImport(imports);
+//        }
     }
 
 }

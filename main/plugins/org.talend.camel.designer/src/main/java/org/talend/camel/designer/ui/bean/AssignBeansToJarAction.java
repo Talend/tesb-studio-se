489166e78eabbdea7d8e9ccbd49d2162af5b1d23
Zhiwei Xue <zwxue@talend.com>
2021-01-25 00:44:39 +0800

    feat(TUP-29014): backport 20210125

--- main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/ui/bean/AssignBeansToJarAction.java
+++ main/plugins/org.talend.camel.designer/src/main/java/org/talend/camel/designer/ui/bean/AssignBeansToJarAction.java
@@ -12,105 +12,38 @@
 // ============================================================================
 package org.talend.camel.designer.ui.bean;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.TreeViewer;
-import org.eclipse.jface.window.Window;
 import org.eclipse.swt.graphics.Image;
-import org.talend.camel.core.model.camelProperties.BeanItem;
-import org.talend.camel.core.model.camelProperties.BeansJarItem;
 import org.talend.camel.designer.util.ECamelCoreImage;
-import org.talend.commons.exception.ExceptionHandler;
-import org.talend.commons.runtime.model.repository.ERepositoryStatus;
 import org.talend.commons.ui.runtime.image.ImageProvider;
-import org.talend.commons.ui.runtime.image.OverlayImageProvider;
 import org.talend.core.model.properties.Item;
-import org.talend.core.model.relationship.RelationshipItemBuilder;
 import org.talend.core.model.repository.ERepositoryObjectType;
-import org.talend.core.model.routines.RoutinesUtil;
-import org.talend.core.repository.model.ProxyRepositoryFactory;
-import org.talend.repository.ProjectManager;
-import org.talend.repository.model.IProxyRepositoryFactory;
-import org.talend.repository.model.RepositoryNode;
-import org.talend.repository.model.RepositoryNodeUtilities;
-import org.talend.repository.ui.dialog.RepositoryReviewDialog;
+import org.talend.repository.ui.actions.routines.AbstractAssignCodesToJarAction;
 
-public class AssignBeansToJarAction extends AbstractBeanAction {
+public class AssignBeansToJarAction extends AbstractAssignCodesToJarAction {
 
-    public AssignBeansToJarAction() {
-        super();
-        setText("Assign Bean to..."); //$NON-NLS-1$
-        setToolTipText("Assign Bean to..."); //$NON-NLS-1$
-
-        Image folderImg = ImageProvider.getImage(ECamelCoreImage.BEAN_ICON);
-        this.setImageDescriptor(OverlayImageProvider.getImageWithNew(folderImg));
+    @Override
+    protected String getActionText() {
+        return "Assign Bean to..."; //$NON-NLS-1$
     }
 
     @Override
-    public void init(TreeViewer viewer, IStructuredSelection selection) {
-        super.init(viewer, selection);
-        boolean canWork = !selection.isEmpty() && selection.size() == 1;
-        IProxyRepositoryFactory factory = ProxyRepositoryFactory.getInstance();
-        if (factory.isUserReadOnlyOnCurrentProject()) {
-            canWork = false;
-        }
-        RepositoryNode node = (RepositoryNode) selection.getFirstElement();
-        if (canWork) {
-            if (node.getObjectType() != ERepositoryObjectType.BEANS
-                    || !ProjectManager.getInstance().isInCurrentMainProject(node) || !isLastVersion(node)) {
-                canWork = false;
-            } else {
-                Item item = node.getObject().getProperty().getItem();
-                if (item instanceof BeanItem && RoutinesUtil.isInnerCodes(item.getProperty())) {
-                    canWork = false;
-                }
-            }
-        }
-        if (canWork) {
-            canWork = (factory.getStatus(node.getObject()) != ERepositoryStatus.DELETED);
-        }
-        setEnabled(canWork);
+    protected Image getImage() {
+        return ImageProvider.getImage(ECamelCoreImage.BEAN_ICON);
     }
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
     @Override
-    protected void doRun() {
+    protected ERepositoryObjectType getTargetCodesJarType() {
+        return ERepositoryObjectType.BEANSJAR;
+    }
 
-        RepositoryReviewDialog dialog = new RepositoryReviewDialog(getWorkbenchPart().getSite().getShell(),
-                ERepositoryObjectType.BEANSJAR);
-        if (dialog.open() == Window.OK) {
-            RepositoryNode targetBeansJarNode = dialog.getResult();
-            ISelection selection = getSelection();
-            Iterator<?> iterator = ((IStructuredSelection) selection).iterator();
-            while (iterator.hasNext()) {
-                RepositoryNode node = (RepositoryNode) iterator.next();
-                IPath targetPath = RepositoryNodeUtilities.getPath(targetBeansJarNode);
-                BeanItem sourceItem = (BeanItem) node.getObject().getProperty().getItem();
-                List backupImports = new ArrayList<>(sourceItem.getImports());
-                sourceItem.getImports().clear();
-                RoutinesUtil.setInnerCodes(sourceItem.getProperty(), ERepositoryObjectType.BEANSJAR);
-                BeansJarItem beansJarItem = (BeansJarItem) targetBeansJarNode.getObject().getProperty().getItem();
-                beansJarItem.getRoutinesJarType().getImports().addAll(backupImports);
-                try {
-                    ProxyRepositoryFactory.getInstance().copy(sourceItem, targetPath, sourceItem.getProperty().getLabel());
-                    ProxyRepositoryFactory.getInstance().save(beansJarItem);
-                    RelationshipItemBuilder.getInstance().addOrUpdateItem(beansJarItem);
+    @Override
+    protected ERepositoryObjectType getCodeType() {
+        return ERepositoryObjectType.BEANS;
+    }
 
-                    // reset
-                    sourceItem.getImports().addAll(backupImports);
-                    RoutinesUtil.setInnerCodes(sourceItem.getProperty(), null);
-
-                    ProxyRepositoryFactory.getInstance().deleteObjectPhysical(node.getObject());
-                } catch (Exception e) {
-                    ExceptionHandler.process(e);
-                }
-            }
-        }
+    @Override
+    protected boolean isTargetItem(Item item) {
+        return "BeanItem".equals(item.eClass().getName()); //$NON-NLS-1$
     }
 
 }
